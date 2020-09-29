0a9bacdc685ade1b303dcabaa7fa2cf79b29c798
moctarjallo <moctarjallo@gmail.com>
2020-09-29 22:36:49 +0000

    Added infor param to read_ methods

--- consolefile/domain/console.py
+++ consolefile/domain/console.py
@@ -2,20 +2,20 @@ from .endpoint import Endpoint
 
 class Console(Endpoint):
 
-    def read(self, variable):
-        value = input(f"Enter your {variable}: ")
+    def read(self, variable, info=''):
+        value = input(f"Enter your {variable}{info}: ")
         return variable, value
 
-    def read_int(self, variable):
-        value = input(f"Enter your {variable}: ")
+    def read_int(self, variable, info=''):
+        value = input(f"Enter your {variable}{info}: ")
         try:
             value = int(value)
             return variable, value
         except Exception:
             raise Exception(f"{variable} should be an integer")
 
-    def read_float(self, variable):
-        value = input(f"Enter your {variable}: ")
+    def read_float(self, variable, info=''):
+        value = input(f"Enter your {variable}{info}: ")
         try:
             value = float(value)
         except Exception:
--- tests/test_console.py
+++ tests/test_console.py
@@ -6,50 +6,50 @@ class TestRead(unittest.TestCase):
     # @unittest.skip('asks user input, but already tested')
     def test_simple(self):
         console = Console()
-        data = console.read('name')
+        data = console.read('name', info='(input a text string)')
         self.assertEqual(data[0], 'name')
         self.assertIsInstance(data[1], str)
 
     def test_varibale_name_not_in_keys(self):
         console = Console()
-        data = console.read('name')
+        data = console.read('name', info='(input a text string)')
         self.assertNotEqual(data[0], 'Name')
         self.assertIsInstance(data[1], str)
 
 class TestReadInt(unittest.TestCase):
     def test_simple(self):
         console = Console()
-        data = console.read_int('age')
+        data = console.read_int('age', info='(input an integer)')
         self.assertEqual(data[0], 'age')
         self.assertIsInstance(data[1], int)
 
     def test_read_float_as_int(self):
         console = Console()
         with self.assertRaises(Exception):
-            console.read_int('balance')
+            console.read_int('age', info='(input a float)')
 
     def test_read_text_as_int(self):
         console = Console()
         with self.assertRaises(Exception):
-            console.read_int('action')
+            console.read_int('action', info='(input a text string)')
 
 class TestReadFloat(unittest.TestCase):
     def test_read_float(self):
         console = Console()
-        data = console.read_float('balance')
+        data = console.read_float('balance', info='(input a float/integer)')
         self.assertEqual(data[0], 'balance')
         self.assertIsInstance(data[1], float)
 
     def test_read_int_to_float(self):
         console = Console()
-        data = console.read_float('age')
+        data = console.read_float('age', info='(input an integer)')
         self.assertEqual(data[0], 'age')
         self.assertIsInstance(data[1], float)
 
     def test_read_text_as_float(self):
         console = Console()
         with self.assertRaises(Exception):
-            console.read_float('action')
+            console.read_float('action', info='(input a text string)')
 
 
 class TestWrite(unittest.TestCase):
