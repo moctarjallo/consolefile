fdd46c64a45fb068105e22f2941b42c4bb1700db
moctarjallo <moctarjallo@gmail.com>
2020-09-29 09:22:38 +0000

    Added basic read operations for console

--- consolefile/domain/console.py
+++ consolefile/domain/console.py
@@ -4,9 +4,23 @@ class Console(Endpoint):
 
     def read(self, variable):
         value = input(f"Enter your {variable}: ")
-        if value.isdigit():
-            value = int(value)
         return variable, value
 
-    def write(self, data):
+    def read_int(self, variable):
+        value = input(f"Enter your {variable}: ")
+        try:
+            value = int(value)
+            return variable, value
+        except Exception:
+            raise Exception(f"{variable} should be an integer")
+
+    def read_float(self, variable):
+        value = input(f"Enter your {variable}: ")
+        try:
+            value = float(value)
+        except Exception:
+            raise Exception(f"{variable} should be float or integer")
+        return variable, value
+
+    def write(self, data, mode=''):
         print(data)
--- tests/test_console.py
+++ tests/test_console.py
@@ -10,17 +10,47 @@ class TestRead(unittest.TestCase):
         self.assertEqual(data[0], 'name')
         self.assertIsInstance(data[1], str)
 
-    def test_int_input(self):
-        console = Console()
-        data = console.read('age')
-        self.assertEqual(data[0], 'age')
-        self.assertIsInstance(data[1], int)
-
     def test_varibale_name_not_in_keys(self):
         console = Console()
         data = console.read('name')
         self.assertNotEqual(data[0], 'Name')
         self.assertIsInstance(data[1], str)
+
+class TestReadInt(unittest.TestCase):
+    def test_simple(self):
+        console = Console()
+        data = console.read_int('age')
+        self.assertEqual(data[0], 'age')
+        self.assertIsInstance(data[1], int)
+
+    def test_read_float_as_int(self):
+        console = Console()
+        with self.assertRaises(Exception):
+            console.read_int('balance')
+
+    def test_read_text_as_int(self):
+        console = Console()
+        with self.assertRaises(Exception):
+            console.read_int('action')
+
+class TestReadFloat(unittest.TestCase):
+    def test_read_float(self):
+        console = Console()
+        data = console.read_float('balance')
+        self.assertEqual(data[0], 'balance')
+        self.assertIsInstance(data[1], float)
+
+    def test_read_int_to_float(self):
+        console = Console()
+        data = console.read_float('age')
+        self.assertEqual(data[0], 'age')
+        self.assertIsInstance(data[1], float)
+
+    def test_read_text_as_float(self):
+        console = Console()
+        with self.assertRaises(Exception):
+            console.read_float('action')
+
 
 class TestWrite(unittest.TestCase):
     # @unittest.skip("prints 'Hello World' successfully")
