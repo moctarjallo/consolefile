9db583c9db380ca3f6fd3729d278a70193449311
moctarjallo <moctarjallo@gmail.com>
2020-09-30 04:01:26 +0000

    Unforget monitoring file operations

--- consolefile/domain/__init__.py
+++ consolefile/domain/__init__.py
@@ -1,2 +1,4 @@
+from .endpoint import Endpoint
+
 from .console import Console
 from .file import File
--- consolefile/domain/file.py
+++ consolefile/domain/file.py
@@ -2,41 +2,17 @@ from .endpoint import Endpoint
 
 class File(Endpoint):
 
-    def __init__(self, filename, title='DATABASE'):
+    def __init__(self, filename):
         self.filename = filename
-        # self.file = open(filename, mode='w+')
-        # self.file.write(f"--------------{filename.upper()} {title}--------------\n")
-        # self.file.close()
 
-    def read(self, variable):
-        self.write('\n\n')
+    def read(self):
         with open(self.filename, 'r') as f:
             return f.readlines()
-            
-    def write(self, save_format):
+
+    def write(self, data):
+        with open(self.filename, 'w+') as f:
+            f.writelines(data)
+
+    def add(self, data):
         with open(self.filename, 'a+') as f:
-            f.write(save_format)
-
-if __name__ == '__main__':
-    f = File('./draft.txt')
-    data = """FIRSTNAME : Moctar
-LASTNAME : Diallo
-ADDRESS : 
-\tCITY: Medina
-BALANCE : 400
-CODE : 5221
-
-FIRSTNAME : Amadou
-LASTNAME : Ba
-ADDRESS : 
-\tCITY: Dakar
-BALANCE : 800
-CODE : 9921
-
-"""
-    # f.write(data)
-    data = f.read()
-    # print(f"readlines:{data}")
-    import pythonapi as api
-    req = api.TextRequest(data)
-    print(req.data)
+            f.writelines(data)
